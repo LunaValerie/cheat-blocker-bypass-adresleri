6b8368d5cc9a5588adf303bfe9237f314ac3184f
LunaUI <59039458+LunaValerie@users.noreply.github.com>
2022-06-10 07:03:27 +0300

    Add files via upload

---
+++ CheatBlocker.cpp
@@ -0,0 +1,18 @@
+Adresleri bulan LunaUI
+Hangi özelikleri Destekliyor peki
+
+CB.cbm : 6A 10 68 78 E2 A9 64 E8 E4 95 01 00 8B 7D 08 85 FF 75 0D
+
+CB.cbm : E8 72 A3 00 00 89 B8 60 03 00 00 E8 7B AE 00 00 84 C0 74 10
+
+CB.cbm : 6A 01 E8 A3 AD 00 00 F7 D8 1A C0 FE C0 88 47 10
+
+CB.cbm : 83 65 FC 00 8B 37 FF 77 04 8B CE FF 15 CC 72 A8 64 FF D6 50 E8 7C 01 00 00 8B 4D EC 8B 01 8B 00 89 45 E0 51 50 E8 BC 84 00 00 59 59 C3
+
+WallHack
+Chams
+NameEsp
+Recoil
+Health Esp
+--------
+string olan hepsini desteklemekte
